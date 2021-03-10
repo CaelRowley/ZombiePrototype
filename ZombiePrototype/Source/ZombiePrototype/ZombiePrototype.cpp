513c643492db8035ecc635e38d170cd858091e17
caelrowley <caelrowley@gmail.com>
2021-03-10 22:13:42 +0100

    feat: add base map for project

---
+++ ZombiePrototype/Source/ZombiePrototype/ZombiePrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZombiePrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZombiePrototype, "ZombiePrototype" );
---
+++ ZombiePrototype/Source/ZombiePrototype/ZombiePrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombiePrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIEPROTOTYPE_API AZombiePrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
