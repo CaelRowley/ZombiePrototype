8f0a38faee297865e64ab9564143cdf08a45a609
caelrowley <caelrowley@gmail.com>
2021-04-07 15:25:47 +0200

    feat: create controller base with logged input mappings

--- ZombiePrototype/Source/ZombiePrototype/Actors/Player/ControllerBase.cpp
+++ ZombiePrototype/Source/ZombiePrototype/Actors/Player/ControllerBase.cpp
@@ -13,3 +13,121 @@ AControllerBase::AControllerBase()
 	MotionController->SetShowDeviceModel(true);
 	SetRootComponent(MotionController);
 }
+
+// Action mappings
+void AControllerBase::TriggerLeftPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TriggerLeftPressed()"));
+}
+
+void AControllerBase::TriggerLeftReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TriggerLeftReleased()"));
+}
+
+void AControllerBase::TriggerRightPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TriggerRightPressed()"));
+}
+
+void AControllerBase::TriggerRightReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TriggerRightReleased()"));
+}
+
+void AControllerBase::GrabLeftPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GrabLeftPressed()"));
+}
+
+void AControllerBase::GrabLeftReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GrabLeftReleased()"));
+}
+
+void AControllerBase::GrabRightPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GrabRightPressed()"));
+}
+
+void AControllerBase::GrabRightReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GrabRightReleased()"));
+}
+
+void AControllerBase::TrackpadLeftPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TrackpadLeftPressed()"));
+}
+
+void AControllerBase::TrackpadLeftReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TrackpadLeftReleased()"));
+}
+
+void AControllerBase::TrackpadRightPressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TrackpadRightPressed()"));
+}
+
+void AControllerBase::TrackpadRightReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("TrackpadRightReleased()"));
+}
+
+// Axis mappings
+void AControllerBase::LeftThumbstickX(float axisValue, APawn* pawn, FVector direction)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("LeftThumbstickX(%f, %s, %s)"), axisValue, *FString(pawn->GetName()), *FString(direction.ToString()));
+	}
+}
+
+void AControllerBase::LeftThumbstickY(float axisValue, APawn* pawn, FVector direction)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("LeftThumbstickY(%f, %s, %s)"), axisValue, *FString(pawn->GetName()), *FString(direction.ToString()));
+	}
+}
+
+void AControllerBase::RightThumbstickX(float axisValue, APawn* pawn, FVector direction)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("RightThumbstickX(%f, %s, %s)"), axisValue, *FString(pawn->GetName()), *FString(direction.ToString()));
+	}
+}
+
+void AControllerBase::RightThumbstickY(float axisValue, APawn* pawn, FVector direction)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("RightThumbstickY(%f, %s, %s)"), axisValue, *FString(pawn->GetName()), *FString(direction.ToString()));
+	}
+}
+
+void AControllerBase::SqueezeLeft(float axisValue)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("SqueezeLeft(%f)"), axisValue);
+	}
+}
+
+void AControllerBase::SqueezeRight(float axisValue)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("SqueezeRight(%f)"), axisValue);
+	}
+}
+
+void AControllerBase::TriggerLeftAxis(float axisValue)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("TriggerLeftAxis(%f)"), axisValue);
+	}
+}
+
+void AControllerBase::TriggerRightAxis(float axisValue)
+{
+	if (axisValue > 0.1 || axisValue < -0.1) {
+		UE_LOG(LogTemp, Warning, TEXT("TriggerRightAxis(%f)"), axisValue);
+	}
+}
--- ZombiePrototype/Source/ZombiePrototype/Actors/Player/ControllerHand.cpp
+++ ZombiePrototype/Source/ZombiePrototype/Actors/Player/ControllerHand.cpp
@@ -6,12 +6,7 @@
 // Sets default values
 AControllerHand::AControllerHand()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	//PrimaryActorTick.bCanEverTick = false;
 
-	//MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionController"));
-	MotionController->SetShowDeviceModel(false);
-	//SetRootComponent(MotionController);
 }
 
 void AControllerHand::PairControllers(AControllerBase* Controller)
@@ -20,89 +15,16 @@ void AControllerHand::PairControllers(AControllerBase* Controller)
 	OtherController->OtherController = this;
 }
 
-// Action mappings
-void AControllerHand::TriggerLeftPressed()
-{
-	UE_LOG(LogTemp, Warning, TEXT("TriggerLeftPressed()"));
-}
-
-void AControllerHand::TriggerLeftReleased()
-{
-	UE_LOG(LogTemp, Warning, TEXT("TriggerLeftReleased()"));
-}
-
-void AControllerHand::TriggerRightPressed()
-{
-	UE_LOG(LogTemp, Warning, TEXT("TriggerRightPressed()"));
-}
-
-void AControllerHand::TriggerRightReleased()
-{
-	UE_LOG(LogTemp, Warning, TEXT("TriggerRightReleased()"));
-}
-
-void AControllerHand::GrabLeftPressed()
-{
-	UE_LOG(LogTemp, Warning, TEXT("GrabLeftPressed()"));
-}
-
-void AControllerHand::GrabLeftReleased()
-{
-	UE_LOG(LogTemp, Warning, TEXT("GrabLeftReleased()"));
-}
-
-void AControllerHand::GrabRightPressed()
-{
-	UE_LOG(LogTemp, Warning, TEXT("GrabRightPressed()"));
-}
-
-void AControllerHand::GrabRightReleased()
-{
-	UE_LOG(LogTemp, Warning, TEXT("GrabRightReleased()"));
-}
 
 // Axis mappings
-
-void AControllerHand::LeftThumbstickX(float throttle, APawn* pawn, UCameraComponent* Camera)
+void AControllerHand::LeftThumbstickX(float axisValue, APawn* pawn, FVector direction)
 {
-	if (throttle != 0) {
-		UE_LOG(LogTemp, Warning, TEXT("MoveRight: %f"), throttle);
-	}
-	pawn->AddMovementInput(throttle * Camera->GetRightVector());
+	Super::LeftThumbstickX(axisValue, pawn, direction);
+	pawn->AddMovementInput(axisValue * direction);
 }
 
-void AControllerHand::LeftThumbstickY(float throttle, APawn* pawn, UCameraComponent* Camera)
+void AControllerHand::LeftThumbstickY(float axisValue, APawn* pawn, FVector direction)
 {
-	if (throttle != 0) {
-		UE_LOG(LogTemp, Warning, TEXT("MoveForward: %f"), throttle);
-	}
-	pawn->AddMovementInput(throttle * Camera->GetForwardVector());
-}
-
-void AControllerHand::RightThumbstickX(float throttle, APawn* pawn, UCameraComponent* Camera)
-{
-	if (throttle != 0) {
-		UE_LOG(LogTemp, Warning, TEXT("MoveRight: %f"), throttle);
-	}
-	pawn->AddMovementInput(throttle * Camera->GetRightVector());
-}
-
-void AControllerHand::RightThumbstickY(float throttle, APawn* pawn, UCameraComponent* Camera)
-{
-	if (throttle != 0) {
-		UE_LOG(LogTemp, Warning, TEXT("MoveForward: %f"), throttle);
-	}
-	pawn->AddMovementInput(throttle * Camera->GetForwardVector());
-}
-
-void AControllerHand::SqueezeLeft(float throttle)
-{
-	UE_LOG(LogTemp, Warning, TEXT("SqueezeLeft(%f)"), throttle);
-}
-
-void AControllerHand::SqueezeRight(float throttle)
-{
-	if (throttle != 0) {
-		UE_LOG(LogTemp, Warning, TEXT("SqueezeRight(%f)"), throttle);
-	}
+	Super::LeftThumbstickY(axisValue, pawn, direction);
+	pawn->AddMovementInput(axisValue * direction);
 }
